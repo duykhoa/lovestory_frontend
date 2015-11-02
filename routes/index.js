dee16ecd2a137240e86f407b71dc90552ab6e863
Duy Khoa <duykhoa12t@gmail.com>
2015-11-02 20:11:52 +0800

    Render config file in nodejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var config = {api_endpoint: 'http://localhost:3000'};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    config: config
+  });
 });
 
 module.exports = router;
