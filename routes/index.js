545bb23a205f852f6663025708454dfbd507e5a5
Duy Khoa <duykhoa12t@gmail.com>
2015-11-03 22:20:14 +0800

    Show home-page element if jwt token in nil

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,12 @@ var config = require('konfig')();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  jwt_token = req.param('jwt_token');
+
   res.render('index', {
     title: 'Love Story',
-    config: config
+    config: config,
+    jwt_token: jwt_token
   });
 });
 
